d6c0adc19ec60eab87d2f5a4f2a2bc759c590bdb
Saras <78587510+sarasverma@users.noreply.github.com>
2022-09-20 20:45:25 +0530

    search bar

--- src/components/Headers/Headers.js
+++ src/components/Headers/Headers.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Headers.css";
+import Searchbar from "./Searchbar";
 
 const Headers = () => {
   return (
     <header>
+      <Searchbar />
       <nav className="navbar" id="navbar">
         <ul className="navList" id="navList">
           <li className="navItems" id="navItems">
---
+++ src/components/Headers/Searchbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import SearchImg from "./img/search.png";
+import "./Searchbar.css";
+
+const Searchbar = () => {
+  return (
+    <div id="searchbar">
+      <div id="logo" className="searchItems">
+        Trade X
+      </div>
+      <div id="search" className="searchItems">
+        <form role="search">
+          <input type="text" placeholder="Search" name="search" />
+          <button type="submit">
+            <img src={SearchImg} type="submit"></img>
+          </button>
+        </form>
+      </div>
+      <div className="userInfo searchItems"></div>
+    </div>
+  );
+};
+
+export default Searchbar;
