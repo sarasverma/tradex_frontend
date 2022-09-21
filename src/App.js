462e0e32be10a5670ade665fdaf8aae2d9d627b1
Saras <78587510+sarasverma@users.noreply.github.com>
2022-09-21 22:20:12 +0530

    Making product card

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import Headers from "./components/Headers/Headers";
 import Footers from "./components/Footers/Footers";
 import Home from "./components/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Headers />
-      <Home />
-      <Footers />
+      <BrowserRouter>
+        <Headers />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+        <Footers />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
+
+const product = {
+  name: "Shirt",
+  images: [
+    {
+      url: "https://upload.wikimedia.org/wikipedia/commons/2/24/Blue_Tshirt.jpg",
+    },
+  ],
+  price: "₹100",
+  _id: "sample_id",
+};
 
 const Home = () => {
   return (
     <div className="container">
-      <h1>Welcome to the trade x</h1>
+      <h2 className="heading">Featured products</h2>
+      <div className="productsContainer">
+        <Product product={product} />
+      </div>
     </div>
   );
 };
